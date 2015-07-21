9bf0a50d0ab6a043c6eb1752b9c0bfe14bf864f1
angelamaunz <alynnem0z@gmail.com>
2015-07-20 18:28:50 -0600

    revised ERD to add memberId as foreign key to listing table.

---
+++ php/member.php
@@ -0,0 +1,53 @@
+<?php
+
+/** require_once */
+
+/**
+ * Small Cross Section of a AirBnb Member
+ *
+ * This -------------------
+ *
+ * @author Angie Maunz <me@example.com>
+ **/
+
+class Member {
+	/** id for this member; This is the primary key
+	 * @var int $userId
+	 **/
+	private $userId;
+
+	/**  birthday entered must be at least 18 years old.
+	 * @var int $birth
+	 **/
+	private $birthday;
+	/** ate (should have time) that account was created.
+	 * @var int $userId
+	 **/
+	private $createDate;
+	/** date (should have time) that account was created.
+	 * @var int $userId
+	 **/
+	private $email;
+	/** id for this member; This is the primary key
+	 * @var int $userId
+	 **/
+	private $firstName;
+	/** id for this member; This is the primary key
+	 * @var int $userId
+	 **/
+	private $lastName;
+	/** id for this member; This is the primary key
+	 * @var int $userId
+	 **/
+	private $loginName;
+	/** id for this member; This is the primary key
+	 * @var int $userId
+	 **/
+	private $password;
+	/** id for this member; This is the primary key
+	 * @var int $userId
+	 **/
+
+
+
+}
