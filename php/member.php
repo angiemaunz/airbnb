94a5fab7d3d8905df2893254b392909fafc02dca
angelamaunz <alynnem0z@gmail.com>
2015-07-21 06:50:24 -0600

    added more detail to use cases per DLM. split phase2 into a separate page

--- php/member.php
+++ php/member.php
@@ -4,23 +4,24 @@
 class Member {
 
 	public $memberId;
-
 	public $birthday;
-
 	public $email;
-
 	public $firstName;
-
 	public $lastName;
-
 	public $loginName;
-
 	public $password;
+	const MINCHARS = 8;
 
 	public function login(){
-	return "logging in ...";
+	return "Logging in ...";
 }
 	public function logout(){
-		return "logging out ...";
+		return "Logging out ...";
+	}
+	public function setPassword ($string)
+	{
+		if (strlen($string) < self::MINCHARS) {
+			throw new Exception('The password should be at least ' . self::MINCHARS . ' characters long.');
+		}
 	}
 }
